56aed0158ce5e70bc12191e3443947e26ed583dc
tortino14 <ricco_mazze7j@icloud.com>
2026-06-12 11:49:52 +0300

    Refactor server.js for improved clarity and fixes

--- server.js
+++ server.js
@@ -4,21 +4,15 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-/* =======================
-   STATIC FILES (FIX CANONICO)
-======================= */
 app.use(express.static(path.join(__dirname, "public")));
 
-/* FIX: homepage esplicita */
+/* 🔥 FIX PRINCIPALE */
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-/* =======================
-   SOCKET.IO MULTIPLAYER
-======================= */
 io.on("connection", (socket) => {
 
     socket.on("joinRoom", ({ room, username }) => {
@@ -31,35 +25,16 @@ io.on("connection", (socket) => {
         });
     });
 
-    socket.on("draw", (data) => {
-        socket.to(data.room).emit("draw", data);
-    });
-
-    socket.on("shape", (data) => {
-        socket.to(data.room).emit("shape", data);
-    });
-
     socket.on("chat", (data) => {
         io.to(data.room).emit("chat", data);
     });
 
-    socket.on("clear", (room) => {
-        io.to(room).emit("clear");
-    });
-
-    socket.on("disconnect", () => {
-        if (socket.room && socket.username) {
-            socket.to(socket.room).emit("chat", {
-                message: `System: ${socket.username} left`
-            });
-        }
+    socket.on("draw", (data) => {
+        socket.to(data.room).emit("draw", data);
     });
 
 });
 
-/* =======================
-   START SERVER (RENDER FIX)
-======================= */
 http.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+    console.log("Server running on " + PORT);
 });
